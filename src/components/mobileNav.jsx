6dc55e4021a7cebd8762b8cee4500303cb88c317
emdevelopa <gyimahsunday@gmail.com>
2025-02-22 22:28:38 +0100

    mobile navbar

--- src/components/mobileNav.jsx
+++ src/components/mobileNav.jsx
@@ -6,7 +6,7 @@ const MobileNav = ({menuOpen}) => {
     return (
       <>
         {menuOpen && (
-          <div className="absolute z-20 top-16 left-0 w-full bg-black text-white p-4 flex flex-col items-center space-y-4 md:hidden">
+          <div className="absolute z-20 top-32 left-0 w-full bg-black text-white p-4 flex flex-col items-center space-y-4 md:hidden">
             <a href="/" className="hover:text-green-400">
               Home
             </a>
@@ -22,8 +22,8 @@ const MobileNav = ({menuOpen}) => {
             <a href="/contact" className="hover:text-green-400">
               Contact
             </a>
-            <div className="hidden md:block bg-white text-black px-4 py-2 rounded-lg shadow-md ">
-              <div className="flex space-x-4 mt-4 md:mt-0">
+            <div className=" md:hidden bg-white text-black px-4 py-2 rounded-lg shadow-md ">
+              <div className="flex space-x-4 mt- md:mt-0">
                 <a href="#" className="text-[#2E3A24] hover:text-[green]">
                   <Facebook size={18} />
                 </a>
